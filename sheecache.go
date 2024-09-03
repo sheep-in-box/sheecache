b63fefece8b40065eb2bdef725cf9d693012e873
sheep_in_box <sheep_in_box@nuaa.edu.cn>
2024-09-03 14:13:18 +0800

    add singleflight.go

--- sheecache.go
+++ sheecache.go
@@ -2,6 +2,7 @@ package sheecache
 
 import (
 	"fmt"
+	"github.com/sheep-in-box/sheecache/singleflight"
 	"log"
 	"sync"
 )
@@ -25,6 +26,9 @@ type Group struct {
 	getter    Getter
 	mainCache cache
 	peers     PeerPicker
+	// use singleflight.Group to make sure that
+	// each key is only fetched once
+	loader *singleflight.Group
 }
 
 var (
@@ -43,6 +47,7 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 		name:      name,
 		getter:    getter,
 		mainCache: cache{cacheBytes: cacheBytes},
+		loader:    &singleflight.Group{},
 	}
 	groups[name] = g
 	return g
@@ -73,16 +78,25 @@ func (g *Group) Get(key string) (ByteView, error) {
 
 // load value for a key from cache
 func (g *Group) load(key string) (value ByteView, err error) {
-	if g.peers != nil {
-		if peer, ok := g.peers.PickPeer(key); ok {
-			if value, err = g.getFromPeer(peer, key); err == nil {
-				return value, nil
+	// each key is only fetched once (either locally or remotely)
+	// regardless of the number of concurrent callers.
+	viewi, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err = g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
+				log.Println("[GeeCache] Failed to get from peer", err)
 			}
-			log.Println("[GeeCache] Failed to get from peer", err)
 		}
+
+		return g.getLocally(key)
+	})
+	if err == nil {
+		return viewi.(ByteView), nil
 	}
 
-	return g.getLocally(key)
+	return
 }
 
 // getLocally value for a key from cache
